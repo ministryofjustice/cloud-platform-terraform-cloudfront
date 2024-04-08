8870908941f03bfb9d4d1c728d68d91487823e64
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-04-08 16:21:05 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,6 +9,10 @@ output "cloudfront_hosted_zone_id" {
 }
 
 output "cloudfront_public_key_ids" {
-  description = "The CloudFront public key IDs"
-  value       = aws_cloudfront_public_key.this[0] ? aws_cloudfront_public_key.this[*].id : null
+  description = "The CloudFront public key IDs, with reference to the public key's first 8 characters."
+  value       = {
+    for key, details in aws_cloudfront_public_key.this:
+      # Offest 27 will skip over the "-----BEGIN PUBLIC KEY-----\n" part of the PEM.
+      key => ({"id" = details.id , "key" = substr(details.encoded_key, 27, 8)})
+  }
 }
