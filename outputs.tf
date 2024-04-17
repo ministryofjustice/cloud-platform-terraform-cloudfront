9956c8674016f5e3a5639ac538900f7f17307810
EarthlingDavey <15802017+EarthlingDavey@users.noreply.github.com>
2024-04-17 15:28:12 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -13,9 +13,9 @@ output "cloudfront_public_keys" {
   value = jsonencode([
     for i in range(length(aws_cloudfront_public_key.this)) :
     {
-      "id"      = aws_cloudfront_public_key.this[i].id
-      "comment" = aws_cloudfront_public_key.this[i].comment
-      "group"   = var.trusted_public_keys[i].associate == true ? aws_cloudfront_key_group.this[0].id : null
+      "id"       = aws_cloudfront_public_key.this[i].id
+      "comment"  = aws_cloudfront_public_key.this[i].comment
+      "group_id" = var.trusted_public_keys[i].associate == true ? aws_cloudfront_key_group.this[0].id : null
     }
   ])
 }
